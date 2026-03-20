f19b9c8a34b017e145773df9b6ca04b6b091c5ad
dazzes4545 <dazzes7619@gmail.com>
2026-03-20 10:55:55 +0300

    Step 6: functions in JavaScript

--- main.js
+++ main.js
@@ -67,3 +67,68 @@ for (let i = 1; i <= 4; i++) {
   }
   console.log(line);
 }
+
+
+function sum(a,b) {
+    return a+b;
+}
+console.log(sum(3,5));
+
+function multiply(x,y) {
+    return x*y;
+}
+console.log(multiply(3,4));
+
+function sayHello(name) {
+    console.log(`hello, ${name}`);
+}
+sayHello("Jake");
+//practice
+function printInfo(name, age) {
+  console.log(`Имя: ${name}, Возраст: ${age}`);
+}
+printInfo("Анна", 25);
+printInfo("Иван", 30);
+
+function greet(name = "Гость") {
+    console.log("Hi" + name);
+}
+greet();
+greet("Neytiri");
+
+//practice
+function calculateDiscount(price, discountPercent = 10) {
+  const discountAmount = price * (discountPercent / 100);
+  const finalPrice = price - discountAmount;
+  return finalPrice;
+}
+console.log(calculateDiscount(1000, 20)); 
+console.log(calculateDiscount(500));       
+console.log(calculateDiscount(2000, 15)); 
+
+const add = function(a,b) {
+    return a+b;
+};
+console.log(add(2,3));
+
+function makeCounter() {
+    let cnt = 0;
+    return function() {
+        count++;
+        return count;
+    };
+}
+
+const counter = makeCounter();
+console.log(counter());
+console.log(counter());
+console.log(counter());
+
+const sumfunc2 = (a,b) => a+b;
+const double = x => x*2;
+const saygreeting = () => console.log("hi");
+const square = x => x*x;
+const calculate = (a,b) => {
+    let res = a+b;
+    return res * 2;
+};
